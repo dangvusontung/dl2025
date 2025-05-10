e221b91006c609cc3432b04458712e43acc3c3a7
Dang Vu Son Tung <dangvusontung@gmail.com>
2025-05-10 22:45:54 +0700

    back prop

--- lab4/components/layer.py
+++ lab4/components/layer.py
@@ -11,3 +11,25 @@ class Layer:
     def forward(self, inputs: list[float]) -> list[float]:
         return self.activate(inputs)
     
+    def get_output(self) -> list[float]:
+        return [neuron.output for neuron in self.neurons]
+    
+    def get_weights(self) -> list[list[float]]:
+        return [neuron.weight for neuron in self.neurons]
+    
+    def get_biases(self) -> list[float]: 
+        return [neuron.bias for neuron in self.neurons]
+    
+    def update_weights(self, learning_rate: float) -> None:
+        for neuron in self.neurons:
+            neuron.update_weight(learning_rate)
+
+    def backward(self, downstream_weights: list[list[float]], downstream_deltas: list[float]) -> None:
+        for i, neuron in enumerate(self.neurons):
+            neuron.compute_hidden_delta(
+                [w[i] for w in downstream_weights], downstream_deltas
+            )
+
+    def compute_output_deltas(self, targets: list[float]) -> None:
+        for neuron, target in zip(self.neurons, targets):
+            neuron.compute_output_delta(target)
--- lab4/components/neuron.py
+++ lab4/components/neuron.py
@@ -1,15 +1,30 @@
 import math
 from typing import Optional
 
-from lab4.helpers import sigmoid
-
+from lab4.helpers import sigmoid, sigmoid_derivative
 
 class Neuron:
-    def __init__(self, weight: Optional[float] = None, bias: Optional[float] = None):
-        self.weight = weight
-        self.bias = bias
+    def __init__(self, weight: Optional[list[float]] = None, bias: Optional[float] = None):
+        self.weight = weight if weight is not None else [0.0]
+        self.bias = bias if bias is not None else 0.0
+        self.output = 0.0
+        self.inputs = []
+        self.delta = 0.0
 
     def activate(self, inputs: list[float]) -> float:
+        self.inputs = inputs
         z = sum([w * i for w, i in zip(self.weight, inputs)]) + self.bias
-        return sigmoid(z)
-    
+        self.output = sigmoid(z)
+        return self.output
+
+    def compute_hidden_delta(self, downstream_weights: list[float], downstream_deltas: list[float]) -> None:
+        weighted_sum = sum(w * d for w, d in zip(downstream_weights, downstream_deltas))
+        self.delta = weighted_sum * sigmoid_derivative(self.output)
+
+    def compute_output_delta(self, target: float) -> None:
+        self.delta = (target - self.output) * sigmoid_derivative(self.output)
+
+    def update_weight(self, learning_rate: float) -> None:
+        for i in range(len(self.weight)):
+            self.weight[i] += learning_rate * self.delta * self.inputs[i]
+        self.bias += learning_rate * self.delta
